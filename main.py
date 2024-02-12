f55a74552540b22d2c5b8d12d8cfbb5b4c186812
darysun07 <sockolenkodar@yandex.ru>
2024-02-12 22:32:16 +0300

    prof

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ app = Flask(__name__)
 @app.route('/list_prof/<list>')
 def list_prof(list):
     with open("templates/list_prof.json", "rt", encoding="utf8") as f:
-        news_list = json.loads(f.read())
-    print(news_list)
-    return render_template('prof.html', list_prof=news_list, list=list)
+        new_list = json.loads(f.read())
+    return render_template('prof.html', list_prof=new_list, list=list)
 
 
 if __name__ == '__main__':
